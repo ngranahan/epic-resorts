cc88ee39ff575c526d0cee32c969d6687676fae6
Nora Granahan <namgranahan@gmail.com>
2021-11-16 14:44:57 -0500

    Adds geocoding for resort locations

--- handler.js
+++ handler.js
@@ -1,12 +1,23 @@
-const {scrape} = require('./helpers')
+const {scrape, getResortLocation, delay, formatData} = require('./helpers')
 const axios = require('axios')
 
-module.exports.getResorts = async (event, context, callback) => {
-  axios('https://www.epicpass.com/')
-  .then(({data}) => {
-    const resortData = scrape(data)
-    console.log('resortData', resortData)
-    callback(null, { resortData })
-  })
-  .catch(callback)
+module.exports.getResorts = async () => {
+  try {
+    const {data} = await axios('https://www.epicpass.com/')
+    const resortData = await scrape(data)
+    if (resortData.resorts && resortData.resorts.length) {
+      for (const [i, resort] of resortData.resorts.entries()) {
+        // HERE geocoding api sets a rate limit of 5 requests per second. Delay by one second every 5th request.
+        if (i%5 === 0) {
+          await delay(1000)
+        }
+        const location = await getResortLocation(resort.resortName, resort.region)
+        resortData.resorts[i].location = location;
+      }
+    }
+    const formattedResortData = formatData(resortData)
+    console.log('formattedResortData', formattedResortData)
+  } catch (e) {
+    console.error(e);
+  }
 };
--- helpers.js
+++ helpers.js
@@ -1,43 +1,75 @@
 const cheerio = require('cheerio')
+const axios = require('axios');
 
-function scrape(html) {
+async function scrape(html) {
   const $ = cheerio.load(html)
   const resortListItems = $('.footer__container').find('.footer__panel__body--resortGroup li')
-
   const resortData = {
-    resorts: [],
-    regions: {}
-  };
+    resorts: []
+  }
 
-  resortListItems.each((i, regionEl) => {
+  for (const regionEl of resortListItems) {
     const region = $(regionEl).find('.panel-title').first().text().trim()
     const regionResorts = $(regionEl).find('.footer__panel__list')
-    regionResorts.each((i, resortEl) => {
+    for (const resortEl of regionResorts) {
       const resortObj = {}
       let resort = $(resortEl).find('.footerlink').first().text().trim()
       resort = resort.split(', opens in a new window')
       resort = resort[0].trim()
       resortObj.resortName = resort
       resortObj.region = region
-      resortObj.location = getResortLocation(resort, region)
       resortData.resorts.push(resortObj)
-      resortData.regions[region] && resortData.regions[region].length ? resortData.regions[region].push(resortObj) : resortData.regions[region] = [resortObj]
-    })
-  })
+    }
+  }
 
   return resortData
 }
 
-function getResortLocation(resort, region) {
-  console.log('geocoding location for:', resort, region)
-  // TODO: Use Google Places API to get resort address from resort name and region
-  // TODO: Use Google Geocoding API to get lat/lng from resort address
-  return {
-    address: 'address',
-    latlng: 'latlng'
+async function getResortLocation(resort, region) {
+  const country = region === 'Canada' || region === 'Australia' ? region : 'USA'
+  const baseUrl = 'https://geocode.search.hereapi.com/v1/geocode'
+  const apiKey = process.env.HERE_API_KEY;
+  const endpointUrl = `${baseUrl}?q=${resort},${country}&limit=1&apiKey=${apiKey}`;
+  try {
+    const locationData = await axios(endpointUrl)
+    const location = {}
+    if (locationData && locationData.data && locationData.data.items && locationData.data.items.length) {
+        location.lat = locationData.data.items[0].position.lat,
+        location.lng = locationData.data.items[0].position.lng,
+        location.hereLocationTitle = locationData.data.items[0].title
+    } else {
+      throw new Error('Error getting results for:', resort, region, endpointUrl)
+    }
+    return location
+  } catch(e) {
+    console.error(e)
   }
 }
 
+function delay(ms) {
+  new Promise(res => setTimeout(res, ms))
+}
+
+function test() {
+  console.log('testing')
+}
+
+function formatData(data) {
+  const formattedData = {
+    resorts: data.resorts,
+    regions: {}
+  }
+
+  data.resorts.forEach(resort => {
+    formattedData.regions[resort.region] && formattedData.regions[resort.region].length ? formattedData.regions[resort.region].push(resort) : formattedData.regions[resort.region] = [resort]
+  })
+
+  return formattedData
+}
+
 module.exports = {
-  scrape
+  scrape,
+  getResortLocation,
+  delay,
+  formatData
 };
