b0feabedf394ee83f0aa7830dfd3b6df2dba8d2e
Nora Granahan <namgranahan@gmail.com>
2021-11-16 11:20:35 -0500

    Adds todos for location data

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-const cheerio = require('cheerio');
+const cheerio = require('cheerio')
 
 function scrape(html) {
   const $ = cheerio.load(html)
@@ -18,13 +18,24 @@ function scrape(html) {
       resort = resort.split(', opens in a new window')
       resort = resort[0].trim()
       resortObj.resortName = resort
-      resortObj.region = region;
+      resortObj.region = region
+      resortObj.location = getResortLocation(resort, region)
       resortData.resorts.push(resortObj)
       resortData.regions[region] && resortData.regions[region].length ? resortData.regions[region].push(resortObj) : resortData.regions[region] = [resortObj]
     })
   })
 
   return resortData
+}
+
+function getResortLocation(resort, region) {
+  console.log('geocoding location for:', resort, region)
+  // TODO: Use Google Places API to get resort address from resort name and region
+  // TODO: Use Google Geocoding API to get lat/lng from resort address
+  return {
+    address: 'address',
+    latlng: 'latlng'
+  }
 }
 
 module.exports = {
